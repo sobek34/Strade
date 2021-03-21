0b65abb59eeaa67403009cb8dbb9a8269676e5cb
unknown <pokrywkasebastian1998@gmail.com>
2021-03-21 12:49:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static('public'))
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+router.get("/admin", (req, res) => {
+  res.render("admin_start");
+});
+
+router.get("/user", (req, res) => {
+  res.render("user_start");
+});
+
+
+router.get("/product", (req, res) => {
+  res.render("product");
+});
+
+
+router.get("/order", (req, res) => {
+  res.render("order");
+});
+
+router.get("/customers", (req, res) => {
+  res.render("customers");
+});
+router.get("/ready_products", (req, res) => {
+  res.render("ready_products");
+});
+router.get("/archives", (req, res) => {
+  res.render("archives");
+});
+
+router.get("/job_description", (req, res) => {
+  res.render("job_description");
+});
+
+router.get("/comments", (req, res) => {
+  res.render("comments");
+});
+
+router.get("/order_user", (req, res) => {
+  res.render("order_user");
+});
+
+
+
+
+
+
+app.use("/", router);
+app.listen(process.env.port || 3000);
+
+console.log("Running at Port 3000");
